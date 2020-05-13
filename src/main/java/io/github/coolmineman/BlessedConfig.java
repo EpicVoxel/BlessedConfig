6270d7c67baa95b069a24a5ed2568073ef8b5f0f
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-05-13 17:07:25 -0500

    add api

---
+++ src/main/java/io/github/coolmineman/BlessedConfig.java
@@ -0,0 +1,92 @@
+package io.github.coolmineman;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
+
+public class BlessedConfig {
+    private boolean isNewConfig;
+    private File file;
+    private FileWriter filewriter = null;
+    private HashMap<String, String> configvalues = new HashMap<>();
+
+    /**
+     * Creates a new config
+     */
+    public BlessedConfig(Identifier configid) {
+        file = new File(FabricLoader.getInstance().getConfigDirectory() + File.separator + configid.getNamespace() + File.separator + configid.getPath() + ".tsv");
+        if (!file.exists()) {
+            (new File(FabricLoader.getInstance().getConfigDirectory() + File.separator + configid.getNamespace())).mkdirs();
+            isNewConfig = true;
+            try {
+                filewriter = new FileWriter(file);
+                filewriter.write("config option\tValue\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            isNewConfig = false;
+        }
+    }
+
+    /**
+     * Adds a config option
+     * @param optionName Name of config option
+     * @param defaultValue 
+     * @return JQuerry style method chaining
+     */
+    public BlessedConfig addOption(String optionName, String defaultValue) {
+        if (isNewConfig && filewriter != null) {
+            configvalues.put(optionName, defaultValue);
+            try {
+                filewriter.write(optionName + "\t" + defaultValue + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Use after creating all your options
+     * @return
+     */
+    public BlessedConfig done() {
+        if (isNewConfig) {
+            try {
+                filewriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                //TSV header
+                reader.readLine();
+                String thisLine = null;
+                while ((thisLine = reader.readLine()) != null) {
+                    String[] parts = thisLine.split("\t");
+                    configvalues.put(parts[0], parts[1]);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Gets value from config file
+     * @param option Name of option
+     * @return value
+     */
+    public String getOption(String option) {
+        return configvalues.get(option);
+    }
+}
--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,8 +1,12 @@
 package net.fabricmc.example;
 
+import io.github.coolmineman.BlessedConfig;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 public class ExampleMod implements ModInitializer {
+	BlessedConfig config;
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -10,5 +14,14 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		System.out.println("Hello Fabric world!");
+		config = new BlessedConfig(new Identifier("modid", "config"))
+			.addOption("TestString", "Jeff")
+			.addOption("TestInt", "57")
+			.addOption("DoubleTest", "6.35")
+			.done();
+
+		System.out.println(config.getOption("TestString"));
+		System.out.println(Integer.parseInt(config.getOption("TestInt")));
+		System.out.println(Double.parseDouble(config.getOption("DoubleTest")));
 	}
 }
